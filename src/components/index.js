913c7034b93a773d2a8a4f53437b9ce2544b0833
Kavan Bhavsar <kvbhavsar35@gmail.com>
2024-11-13 16:26:58 +0530

    Updates

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Button from './Button';
-import Nav from './Nav';
-import ShoeCard from './ShoeCard';
-import PopularProductCard from './PopularProductCard';
-import ServiceCard from './ServiceCard';
-import CustomerCard from './CustomerCard';
-
-
-export { Button, Nav, ShoeCard, PopularProductCard, ServiceCard, CustomerCard };
